acaa5cc6c4f52129beb558fd2100de1bc91c83c4
Shrikant Ther <shrikantther9@gmail.com>
2017-02-02 10:09:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 var element=document.getElementById("main-text");
 
 element.innerHTML="New Value";
+
+var img=document.getElementById("img");
+img.onClick=function(){
+  img.style.marginLeft="100px";  
+    
+};
