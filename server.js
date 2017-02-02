77bdd442564bdba093340f13a0918f79c5864e7e
Shrikant Ther <shrikantther9@gmail.com>
2017-02-02 15:44:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,6 +68,11 @@ return htmltemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var counter=0;
+app.get('/counter',function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+});
 
 //app.get('/articleName',function(req,res){
 // app.get('/article-one',function(req,res){
