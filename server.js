78033fa0cc45316099f38d1dcad7df3672a1b5ec
Shrikant Ther <shrikantther9@gmail.com>
2017-02-22 16:41:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool=require("pg").Pool;
+var config={
+    user:'shrikantther',
+    database: 'shrikantther',
+    host:'db.imad.hasura-app.io',
+    port:'5432'
+password:process.env.DB_PASSWORD
+    
+};
 var app = express();
 app.use(morgan('combined'));
+
+
+
 /*var articles={
  'article-one' : {
     title:'article one',
@@ -19,6 +30,9 @@ app.use(morgan('combined'));
   `    
  }
 };
+
+
+
 function createtemplate(data){
     var date=data.date;
     var title=data.title;
@@ -68,10 +82,23 @@ return htmltemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 var counter=0;
 app.get('/counter',function(req,res){
     counter=counter+1;
     res.send(counter.toString());
+});
+var pool = new Pool(config)
+app.get('/test-db',function(req,res){
+    pool.query('SELECT * FROM test',function(err,result){
+       if(err)
+       {
+           res.statues(500).send(err.toString());
+           
+       }else{
+           res.send(JSON.stringify(result));
+       }
+    });
 });
 
 //app.get('/articleName',function(req,res){
