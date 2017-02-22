a9e6342b81fe69b5c56db68bf102bb7b8524e831
Shrikant Ther <shrikantther9@gmail.com>
2017-02-22 16:49:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var config={
     user:'shrikantther',
     database: 'shrikantther',
     host:'db.imad.hasura-app.io',
-    port:'5432'
+    port:'5432',
 password:process.env.DB_PASSWORD
     
 };
