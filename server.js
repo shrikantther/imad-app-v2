80987d095407e3603c531c3f7f7a7ec022b07f1a
Shrikant Ther <shrikantther9@gmail.com>
2017-02-22 21:25:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.get('/test-db',function(req,res){
     });
 });
 
-app.get('/article/:articleName',function(req,res){
+app.get('/articles/:articleName',function(req,res){
 // app.get('/article-one',function(req,res){
   //  var articleName=req.params.articleName;
    // var articleData=
