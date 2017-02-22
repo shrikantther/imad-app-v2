c722fa289109585be1fe0b6dd4f0b38c32382eee
Shrikant Ther <shrikantther9@gmail.com>
2017-02-22 21:22:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,10 +101,27 @@ app.get('/test-db',function(req,res){
     });
 });
 
-//app.get('/articleName',function(req,res){
+app.get('/article/:articleName',function(req,res){
 // app.get('/article-one',function(req,res){
-  //  res.send(article(article-one)); 
-//});
+  //  var articleName=req.params.articleName;
+   // var articleData=
+   pool.query("SELECT * FROM article where title="+req.params.articleName,function(err,result){
+      if(err)
+      {
+          req.status(500).send(err.toString());
+      }else{
+          if(result.rows.length===0){
+              res.status(404).send('article not found');
+          }else{
+              var articleData=result.rows[0];
+              res.send(createTemplate(articleData));
+          }
+      }
+   });
+  //  res.send(createTemplate(articleData)); 
+});
+
+
 app.get('/article-two',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html')); 
 });
