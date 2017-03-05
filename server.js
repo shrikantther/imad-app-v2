9eea2aec2264eaaaac072401ecec4dd8618472f8
Shrikant Ther <shrikantther9@gmail.com>
2017-03-05 15:12:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool=require("pg").Pool;
 var crypto=require('crypto');
+var bodyParser=require('body-parser');
 var config={
     user:'shrikantther',
     database: 'shrikantther',
@@ -13,8 +14,7 @@ var config={
 };
 var app = express();
 app.use(morgan('combined'));
-
-
+app.use(bodyParser.json());
 function hash(input,salt){
     var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
 
@@ -25,7 +25,21 @@ function hash(input,salt){
      res.send(hashedString);
      
  });
-
+app.post('/create-user',function(req,res){
+    var username=req.body.username;
+    var password=req.body.password;
+    var salt=crypto.getRandomBytes(128).toString('hex');
+    var dbString=hash(password,salt);
+    pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,dbString],function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send('user created: '+ username);
+        
+        }
+    });
+});
 /*var articles={
  'article-one' : {
     title:'article one',
