0ff3ba2b8bcff083208d1aacd251b82c93ac4449
Shrikant Ther <shrikantther9@gmail.com>
2017-03-07 09:34:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,4 @@
-//counter code here
 var button=document.getElementById('counter');
-//var counter=0;
-//submit name
 
 var submit=document.getElementById('submit_btn');
  
@@ -16,34 +13,24 @@ button.onclick =function(){
            if(request.status === 200)
            {
               // var names=['name1','name2','name3','name4'];
-              var names=request.responseText;
-              names=JSON.parse(names);
-    var list='';
-    for(i=0;i<names.length;i++){
-        list+='<li>'+ names[i] +'</li>';
-    }
-       var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
-  
-               //var counter=request.responseText;
-            //   var span=document.getElementById('count');
-             //  span.innerHTML=counter.toString();
+             
+  console.log("user logged in");
+  alert("logged in successfully");
+           }else if(request.status===403){
+              alert("username/password is incorrect") ;
+           }else if(request.status===500){
+               alert("Something went wrong on the server");
            }
            
        }
     };
-    //render of variable in span
-//make request
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
-request.open('GET','http://shrikantther.imad.hasura-app.io/submit-name?name='+name,true);
-request.send(null);
-};
-
-
- 
- 
-   
+var username=document.getElementById('username').value;
+var password=document.getElementById('password').value;
+console.log(username);
+console.log(password);
+request.open('POST','http://shrikantther.imad.hasura-app.io/login',true);
+request.setRequestHeader('Content-Type','application/json');
+request.send(JSON.stringify({username: username,password}));
 };
 
 
